96e6607eae11a751ca7ff7488a9479ad84c8e2fe
root <yohan.p002@gmail.com>
2022-12-11 02:05:22 +0100

    mariadb started

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-if [ ! -d /var/lib/mysql/$DB_NAME ]; then
+if [ ! -d /var/lib/mysql/$MYSQL_DATABASE ]; then
   mysql_install_db
   /usr/share/mariadb/mysql.server start
   mysql -e "\
-    CREATE DATABASE IF NOT EXISTS $DB_NAME; \
-    CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY 'nani'; \
-    GRANT ALL ON $DB_NAME.* TO '$MYSQL_USER'@'%'; \
+    CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; \
+    CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; \
+    GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%'; \
     ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; \
     FLUSH PRIVILEGES; \
     "
